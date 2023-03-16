fa6ceb8225e69ee63feb2c86de3428a08e9ae8c8
Dan <danaaboston@gmail.com>
2023-03-16 16:55:53 -0400

    Updated /client/src-sw.js to implement asset caching and removed unnecessary comments from /client/webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-
 module.exports = () => {
   return {
     mode: 'development',
