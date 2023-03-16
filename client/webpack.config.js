fd319a92adcbc111b35abd6afd5befe5d9060820
Dan <danaaboston@gmail.com>
2023-03-16 15:39:15 -0400

    Updated /client/webpack.config to include CSS loader and fixed formatting of name in /client/package.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,26 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel-transform-runtime']
+            }
+          }
+        }
       ],
     },
   };
