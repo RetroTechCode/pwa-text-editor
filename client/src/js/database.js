09c0270884d3b57a5d5e27333edfa36e97853016
Dan <danaaboston@gmail.com>
2023-03-18 12:16:05 -0400

    Began setting up /client/src/js/database.js. Began adding functionality for accepting content to add to the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,6 +14,13 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
+const textDB = await openDB('jate', 1);
+const tx = textDB.transaction('jate', 'readwrite');
+const store = tx.objectStore('jate');
+const request = store.put({ id: id, text: content });
+const result = await request;
+
+console.log('Successfully saved data to the database!', result);
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
