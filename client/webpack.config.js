6184146499e3dabcdcf14c8933991ff0d76c5fd6
Dan <danaaboston@gmail.com>
2023-03-19 12:40:47 -0400

    Minor changes to webpack.config.js in attempt to fix Heroku issues

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -33,8 +33,8 @@ module.exports = () => {
         description: 'Take notes with JavaScript syntax highlighting!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
-        start_url: './',
-        publicPath: './',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
